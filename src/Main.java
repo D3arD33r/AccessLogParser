78fb14a8896e8baefdd338e402b8f4f83ce51880
D3arD33r <ohhh.artyom@gmail.com>
2025-06-19 23:35:17 +0300

    Напишите код, который будет вычислять и выводить в консоль сумму, разность, произведение и частное двух  чисел вводимых в консоль

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста" + text.length());
+
+        System.out.println("Введите первое число:");
+        int numberFirst = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число:");
+        int numberSecond = new Scanner(System.in).nextInt();
+
+        int sum = numberFirst + numberSecond;
+        int subtraction = numberFirst - numberSecond;
+        int multiplication = numberFirst * numberSecond;
+        double division =(double) numberFirst / numberSecond;
+
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + subtraction);
+        System.out.println("Произведение: " + multiplication);
+        System.out.println("Частное: " + division);
+
     }
 }
