a58ee99ea68e29c986787259d3b57cceefe24a7a
D3arD33r <ohhh.artyom@gmail.com>
2025-08-18 13:34:01 +0300

    Курсовой проект.  Задание #2. Сравнение объектов

--- src/Main.java
+++ src/Main.java
@@ -2,64 +2,76 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
         int countLines = 1;
 
-        System.out.print("Введите путь к файлу: ");
-
-        while (true) {
-
-            String path = new Scanner(System.in).nextLine();
-
-            try { // начало большого цикла try-catch
-
-                FileReader fileReader = new FileReader(path);
-                BufferedReader reader =
-                        new BufferedReader(fileReader);
-                String line;
-
-                int min = Integer.MAX_VALUE;
-                int max = Integer.MIN_VALUE;
-
-                while ((line = reader.readLine()) != null) {
-
-                    int length = line.length();// длинка строки
-
-                    if(length >1024)
-                    {throw new RuntimeException("Ошибка в строке " +countLines +" длина строке = " + length);} //ошибка в строке с символами >1024
+        String path = new Scanner(System.in).nextLine();
 
 
-                    System.out.println("Длина строки: " + length);
+        // создаём счётчики запросов
+        long totalRequests = 0;
+        long googlebotCount = 0;
+        long yandexBotCount = 0;
 
-                    countLines++;
+        // Создаем объекты-константы для сравнения
+        final String GOOGLEBOT = "GoogleBot";
+        final String YANDEXBOT = "YandexBot";
 
+        try  {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader =
+                    new BufferedReader(fileReader);
 
-                    if (length < min) { // проверки на минимально и масимальное значения
-                        min = line.length();
-                    } else if (length > max) {
-                        max = line.length();
-                    }
+            String line;
 
+            while ((line = reader.readLine()) != null) {
+                totalRequests++;
 
+                int length = line.length();// длинка строки
+
+                // проверка ошибка в строке с символами >1024
+                if (length > 1024) {
+                    throw new RuntimeException("Ошибка в строке " + countLines + " длина строке = " + length);
                 }
-                System.out.println("Количество строк: " + countLines);
-                System.out.println("Минимальная длина строки: " + min);
-                System.out.println("Масксимальная длина строки: " + max);
-
-            }
-            catch (FileNotFoundException  ex)
-            {System.out.println("Файл не существует или это папка");
-                ex.printStackTrace();
-            }
-            catch (IOException ex) {
-                System.out.println("Ошибка чтения");
-                ex.printStackTrace();
 
 
-        } catch (RuntimeException ex) {
-                System.out.println(ex);
-                ex.printStackTrace();
+                // Извлечение содержимого первых скобок
+                int start = line.indexOf('(');
+                if (start <= 0) continue;
+                int end = line.indexOf(')', start); // Ищем закрывающую скобку, начиная с позиции start
+                if (end <= 0) continue;
+
+                String firstBrackets = line.substring(start + 1, end);// Берём текст внутри скобок от первой позиции (  равной 0 = start + 1 символ  ДО последней позиции равной ) = end
+                String[] parts = firstBrackets.split(";");// Делит строку на части по ';' по parts
+
+                // Обработка User-Agent только при наличии второй части
+                if (parts.length >= 2) { //  если частей больше 2 по parts
+                    String fragment = parts[1].trim(); // Убирает пробелы: " Android" → "Android"
+                    String botName = fragment.split("/")[0]; // Делит по '/' и берёт первый элемент
+
+                    // Сравнение через hashCode() и equals()
+
+
+                    if (botName.equalsIgnoreCase(GOOGLEBOT)) {
+                        googlebotCount++;
+                    }
+                    else if (botName.equalsIgnoreCase(YANDEXBOT)) {
+                        yandexBotCount++;
+                    }
+                }
             }
+        } catch (IOException | RuntimeException e) {
+            throw new RuntimeException(e);
         }
+
+        // Расчет и вывод результатов
+        long totalBots = googlebotCount + yandexBotCount;
+        double ratio =  (double) totalBots / totalRequests;
+
+        System.out.println("Общее количество запросов: " + totalRequests);
+        System.out.println("Запросов от Googlebot: " + googlebotCount);
+        System.out.println("Запросов от YandexBot: " + yandexBotCount);
+        System.out.printf("Доля запросов от ботов:" +  ratio);
     }
 }
